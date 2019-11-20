019cbfc4e925a4a25c0367e56f178e958b0a8252
Stuart Sears <stuart@sjsears.com>
2019-11-20 17:58:44 +0000

    added api for generating in different formats

--- views.py
+++ views.py
@@ -1,10 +1,19 @@
-from flask import render_template, request, url_for, Blueprint, current_app
+from flask import (
+    render_template,
+    request,
+    url_for,
+    Blueprint,
+    current_app,
+    make_response
+    )
 
 from diagram import UkuleleChord, GuitarChord, MultiFingerChord
-
 from forms import ChordForm, DownloadForm
+
+import cairosvg
+from wand.image import Image
+
 import yaml
-import json
 
 
 my_view = Blueprint('my_view', __name__)
@@ -48,7 +57,7 @@ def home():
         return filename
 
     chord_form = ChordForm(request.form)
-    # download_form = DownloadForm(request.form)
+    dl_form = DownloadForm(request.form)
 
     if request.method == 'POST':
         # Diagram
@@ -57,15 +66,69 @@ def home():
                 'index.html',
                 diagram=create_diagram(chord_form),
                 chord_form=chord_form,
-                # download_form=download_form
+                dl_form=dl_form
             )
 
     return render_template(
                 'index.html',
                 diagram=create_diagram(chord_form),
                 chord_form=chord_form,
-                # download_form=download_form
+                dl_form=dl_form
             )
 
+@my_view.route('/download', methods=['POST'])
+def download_as():
+    if request.method == 'POST':
+        if format == 'png_t':
+            # convert using cairosvg
+            result = cairosvg.svg2png(imgdata)
+        elif format == 'png':
+            i = wand.image.Image(blob=imgdata)
+            result = i.convert('png').make_blob()
+        else:
+            result = imgdata
 
+        return send_file(io.BytesIO(imgdata), as_attachment=True, filename='chord.svg')
+
+@my_view.route('/api/v1/chord/', methods=['POST'])
+def generate_chord():
+    """
+    process the request body and return SVG content (no XML header)
+    """
+    content = request.get_json(silent=True)
+    chordobj = MultiFingerChord(
+            positions=content.get('positions', '0000'),
+            fingers=content.get('fingers','----'),
+            barre=content.get('barre', None),
+            title=content.get('title', 'Am7'),
+            extras=content.get('extras', None),
+            style=chord_style
+            ).render()
+    chordobj.seek(0)
+    header, data = chordobj.read().splitlines()
+
+    # export type
+    fmt = content.get('format', 'svg')
+
+    if fmt != 'svg':
+        content_type = 'image/png'
+        ext = 'png'
+    else:
+        content_type = 'image/svg+xml'
+        ext = 'svg'
+
+    # OK, we have a chord object, let's return it
+    if fmt == 'png_t':
+        # do cairo stuff
+        data = cairosvg.svg2png(bytestring=data.encode('utf-8'))
+
+    if fmt == 'png':
+        with Image(blob=data.encode('utf-8')) as i:
+            data = i.convert('png').make_blob()
+
+    response = make_response(data)
+    response.headers.set('Content-Type', content_type)
+    response.headers.set('Content-Disposition', 'attachment', filename="{}.{}".format(content.get('title'), ext))
+
+    return response
 
